b5b4bf24b443c5768f973357e2a9b90047e5976d
Niels Tanis <nielstanis@live.nl>
2023-02-20 17:16:52 +0100

    Content

---
+++ ConsoleAppWasi/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello DotNetFlix from {RuntimeInformation.OSArchitecture}!");
+
+var content = System.IO.File.ReadAllText("/etc/hosts");
+
+Console.WriteLine(content);
---
+++ Extend/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Hello, DotNetFlix!");
+
+Directory.CreateDirectory("log");
+Wasmtime.WasiConfiguration conf = new Wasmtime.WasiConfiguration()
+    .WithStandardError("log/error.log")
+    .WithStandardOutput("log/output.log")
+    .WithArg("CalledFromConsole")
+    .WithArgs(args); //passing args
+
+var engineConfig = new Wasmtime.Config();
+// engineConfig.WithFuelConsumption(true);
+var engine = new Wasmtime.Engine(engineConfig);
+var linker = new Wasmtime.Linker(engine);
+linker.DefineWasi();
+var store = new Wasmtime.Store(engine);
+store.SetWasiConfiguration(conf);
+// store.AddFuel(5000);
+
+string wasm = @"wasm/myrustmodule.wasm";
+var module = Wasmtime.Module.FromFile(engine, wasm);
+var inst = linker.Instantiate(store, module);
+var main = inst.GetFunction("main");
+
+if (main!=null)
+    main.Invoke(0,0);
+
+//Console.WriteLine($"Consumed fuel: {store.GetConsumedFuel()}");
